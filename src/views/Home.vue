7c962520a498583b296ccaebd8b4c85460a8d618
wanghaobing <wanghaobing@outlook.com>
2019-01-08 10:37:31 +0800

    按需引入 Element UI

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,9 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <p>Element UI： <el-button>按钮</el-button></p>
+    </div>
   </div>
 </template>
 
